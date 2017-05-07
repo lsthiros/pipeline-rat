0a95f1906b24dedd992add62886170b4ab190087
Chris Gerdom <cgerdom@calpoly.edu>
2017-05-06 17:11:57 -0700

    changed to async logic for read sync for write

--- sources_1/new/address_cache.sv
+++ sources_1/new/address_cache.sv
@@ -1,102 +1,150 @@
 `timescale 1ns / 1ps
+`default_nettype none
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
+// Company:
+// Engineer:
+//
 // Create Date: 05/02/2017 03:02:32 PM
-// Design Name: 
+// Design Name:
 // Module Name: address_cache
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
+// Project Name:
+// Target Devices:
+// Tool Versions:
+// Description:
+//
+// Dependencies:
+//
 // Revision:
 // Revision 0.01 - File Created
 // Additional Comments:
-// 
+//
 //////////////////////////////////////////////////////////////////////////////////
 
-parameter INDEX_WIDTH = 4;
-parameter TAG_SIZE = 10 - INDEX_WIDTH;
-parameter CACHE_SIZE = 1 << INDEX_WIDTH;
 
 
-/* defining row of cache */
 
 
-/* typedef struct{
-	logic valid_bit;
-	logic [(TAG_SIZE - 1):0] tag;
-	logic [2:0] history;
-} cache_row; */
-
-module cache_row(
-	input clk,
-	input rst,
-	input we,
-	input branch_taken,
+module TagBlock(
+	input  wire clk,
+	input  wire [5:0] in_tag,
+	input  wire tag_we,
+	input  wire rst,
 	output logic valid_bit,
-	output logic[(TAG_SIZE - 1):0] tag,
-	output logic[2:0] history
+	output logic [5:0]  tag
 	);
-	
+
 	always @ (posedge(clk)) begin
 		if(rst) begin
-			history <= 0;
-			valid_bit 	<= 0;
+			tag <= 0;
+			valid_bit <= 0;
 		end else begin
-			if(we)begin
-				history <= {history[1:0],branch_taken};
-				valid_bit	<= 1;
-			end else
-				history <= history;
-				valid_bit	<= 1;
-			end
+			tag <= (tag_we)? in_tag : tag;
+			valid_bit <= (tag_we)? 1 : valid_bit;
+		end
 	end
-	
-endmodule		
-
-
-module address_cache(
-    input [9:0] pc,
-    input [9:0]update_pc,
-    input branch_taken,
-    input we,
-    input clk,
-    input rst,
-    input branch_detected,
-    output evict,
-    output [2:0] history
-    );
-    
-	wire cache_index = pc [(INDEX_WIDTH - 1):0];
-	wire tag_input = pc [9:INDEX_WIDTH];
-	logic [2:0] ind_history [0:CACHE_SIZE-1];
-	logic ind_we [0:CACHE_SIZE-1];
-	
-	cache_row  cache[0:CACHE_SIZE-1] (
-	   .clk          ( clk ),
-	   .rst          ( rst ),
-	   .we           ( ind_we ),
-	   .branch_taken ( branch_taken ),
-	   .history      ( ind_history )
-
-	);
-	
-	assign history = ind_history[integer'(cache_index)][3]; /*TODO: find out what goes here*/
-	
-	always @ (posedge(clk)) ind_we[integer'(cache_index)] <= we ;/*TODO: find out what goes here*/
-    
-    
-    
-    
-    
-    
-    
-    
-    
 endmodule
 
+module HisBlock(
+	input wire clk,
+	input wire rst,
+	input wire we,
+	input wire branch_taken,
+	output logic [2:0] history
+	);
+
+	always @ ( posedge(clk) ) begin
+		if( rst ) begin
+			history <= {1'b0,1'b0,branch_taken};
+		end
+		else begin
+			if( we ) begin
+				history <= {history[1:0],branch_taken};
+			end
+			else begin
+			history <= history;
+			end
+		end
+	end
+
+endmodule
+
+
+module cache(
+	input wire rst,
+	input wire clk,
+	input wire we,
+	input wire branch_taken,
+	input wire [9:0] pc,
+	input wire [9:0]	update_pc,
+	output logic [2:0] history,
+	output logic read_hit
+	);
+	parameter ENTRY_WIDTH = 4;
+	parameter ENTRY_DEPTH = 1 << ENTRY_WIDTH;
+	wire [5:0] ind_tag[0:ENTRY_DEPTH - 1];
+	wire ind_valid_bit[0:ENTRY_DEPTH - 1];
+	wire [5:0] in_tag = pc[9:4];
+	wire [3:0] index = update_pc[3:0];
+	wire valid_bit;
+	logic ind_history_write  [0:ENTRY_DEPTH - 1];
+	logic ind_tag_we[0:ENTRY_DEPTH - 1];
+	logic ind_rst [0:ENTRY_DEPTH - 1];
+	logic [2:0] ind_history [0:ENTRY_DEPTH - 1];
+	logic [5:0] out_tag;
+
+	TagBlock tagtable [0:ENTRY_DEPTH - 1] (
+		.clk(clk),
+		.in_tag(in_tag),
+		.tag_we(ind_tag_we),
+		.rst(rst),
+		.valid_bit(ind_valid_bit),
+		.tag(ind_tag)
+	);
+
+	HisBlock histable [0:ENTRY_DEPTH - 1] (
+		.clk(clk),
+		.rst(ind_rst),
+		.we(ind_history_write),
+		.branch_taken(~rst && branch_taken),
+		.history(ind_history)
+	);
+
+
+	//sync write section
+    logic write_hit;
+
+	assign out_tag = ind_tag[integer'(index) ];
+	assign write_hit = (in_tag == out_tag) && ind_valid_bit[integer'(index)];
+
+	always @ ( posedge(clk) ) begin
+		if (write_hit && we) begin	// chache hit
+			ind_history_write[integer'(index) ] <= 1;
+			ind_tag_we [integer'(index) ]<= 1;
+			ind_rst[integer'(index) ] <= 0;
+		end
+		else begin
+			if (~write_hit && we) begin  // cache miss
+				ind_history_write[integer'(index) ] <= 0;
+				ind_tag_we [integer'(index) ]<= 1;
+				ind_rst [integer'(index) ] <= (index - 1);
+			end
+			else begin	// no cache write
+				ind_history_write[integer'(index) ] <= 0;
+				ind_tag_we[integer'(index) ] <= 0;
+				ind_rst [integer'(index) ]<= 0;
+			end
+		end
+	end
+
+
+	// read section
+	wire read_index = pc[3:0];
+	assign read_hit = (in_tag == out_tag) && ind_valid_bit[integer'(index)];
+	assign history = ind_history[integer'(read_index)];
+
+
+
+
+
+
+endmodule
