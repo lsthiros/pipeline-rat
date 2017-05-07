a22725e5ad11f98af7679e20161d625a5f4f0376
Chris Gerdom <cgerdom@calpoly.edu>
2017-05-07 16:09:00 -0700

    made some changes to PredictionTables.sv

--- sources_1/new/PredictionTables.sv
+++ sources_1/new/PredictionTables.sv
@@ -1,22 +1,22 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
+// Company:
+// Engineer:
+//
 // Create Date: 05/02/2017 02:46:24 PM
-// Design Name: 
+// Design Name:
 // Module Name: PredictionTables
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
+// Project Name:
+// Target Devices:
+// Tool Versions:
+// Description:
+//
+// Dependencies:
+//
 // Revision:
 // Revision 0.01 - File Created
 // Additional Comments:
-// 
+//
 //////////////////////////////////////////////////////////////////////////////////
 
 
@@ -35,25 +35,25 @@ module PredictionTables(
     input wire [2:0] update_history,
     output wire prediction
     );
-    
+
     parameter ENTRY_WIDTH = 3;
     parameter ENTRY_DEPTH = 1 << ENTRY_WIDTH;
-    
+
     wire table_index = pc[2:0];
     wire update_table_index = old_pc[2:0];
-    
+
     logic ind_rst [0:ENTRY_DEPTH - 1];
     logic [1:0] ind_prediction [0:ENTRY_DEPTH - 1];
     logic ind_update [0:ENTRY_DEPTH - 1];
-    
+
     generate
         genvar idx;
         for (idx = 0; idx < 8; idx++) begin
-            assign ind_update[integer'(idx)] = (integer'(old_pc) == integer'(idx) && we);
-            assign ind_rst[integer'(idx)] = (integer'(evict_idx) == integer'(idx) || rst);
+            assign ind_update[integer'(idx)] = (integer'(update_table_index) == integer'(idx) && we); // TODO: check that old_pc is right
+            assign ind_rst[integer'(idx)] = (integer'(update_table_index) == integer'(idx) || rst);
         end
     endgenerate
-    
+
     RecTable tables [0:ENTRY_DEPTH - 1] (
         .clk(clk),
         .rst(ind_rst),
@@ -63,6 +63,10 @@ module PredictionTables(
         .wr_addr(update_history),
         .taken(branch_taken)
     );
-    
+
     assign prediction = ind_prediction[integer'(table_index)][1];
+
+
+
+
 endmodule
