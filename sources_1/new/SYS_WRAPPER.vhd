2391ba70fec58690fc652b1de66db6be86d50b61
Chris Gerdom <cgerdom@calpoly.edu>
2017-06-08 14:49:52 -0700

    adding the pll to reduce clock speed

--- sources_1/new/SYS_WRAPPER.vhd
+++ sources_1/new/SYS_WRAPPER.vhd
@@ -26,9 +26,17 @@ signal s_clk_div     : STD_LOGIC := '0';
 signal s_sseg_cr     : STD_LOGIC_VECTOR(7 downto 0)  := X"00";
 signal s_sseg_count1 : STD_LOGIC_VECTOR(13 downto 0) := "00" & X"000";
 signal s_sseg_count2 : STD_LOGIC_VECTOR(7 downto 0)  := X"00";
+signal mult_clk : std_logic;
 
 signal s_interrupt   : STD_LOGIC := '0';
 
+
+component clk_wiz_0
+    port(clk_in1    : in std_logic;
+         clk_out1   : out std_logic;
+         reset      : in std_logic;
+         locked     : out std_logic);
+end component;
 -- Declare RAT_wrapper ------------------------------------------------------
 component RAT_pipeline_wrapper
     Port ( LEDS      : out   STD_LOGIC_VECTOR (15 downto 0);
@@ -70,7 +78,7 @@ rat: RAT_pipeline_wrapper
         SWITCHES  => SWITCHES,
         INT       => s_interrupt,
         RST       => RST,
-        CLK       => CLK);
+        CLK       => mult_clk);
 ------------------------------------------------------------------------------
 
 -- Instantiate MAIN_CLK_DIV --------------------------------------------------
@@ -79,7 +87,13 @@ div: MAIN_CLK_DIV
         CLK  => CLK,
         SCLK => s_clk_div);
 ------------------------------------------------------------------------------
-
+mult: clk_wiz_0
+    port map (
+    clk_in1  => CLK,
+    clk_out1 => mult_clk,
+    reset    => RST
+    
+    );
 -- Instantiate db_1shot_FSM --------------------------------------------------              
    my_debouncer: db_1shot_FSM
    port map ( A    => INT,
